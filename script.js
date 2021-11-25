0321b65a6656c231d2f932b7b3dcbfc30d50d7e6
Kathy Coffee <klcoffee8807@gmail.com>
2021-11-25 09:50:05 -0500

    Complete Loops exercise with comments from review

--- script.js
+++ script.js
@@ -22,11 +22,15 @@ for (item of wizards){
 }
 // 4a
 let harryPotterMovies = 0;
-// 4b 4c
-while (harryPotterMovies <=7){
-    console.log(harryPotterMovies++);
+// // 4b 4c my answer
+// while (harryPotterMovies <=7){
+//     console.log(harryPotterMovies++);
+// }
+// 4b Correct answer 
+while (harryPotterMovies <8){
+    harryPotterMovies++;
 }
-
+// 4c
 console.log(harryPotterMovies);
 
 // Bonus
@@ -37,7 +41,7 @@ const hogwartsHouses = [
     `Ravenclaw`,
     `Slytherin`
 ];
-// 5b
+// 5b didn't need 2 consolelogs
 for (titles of hogwartsHouses){
     console.log(titles);
  for (char of titles){
@@ -47,10 +51,31 @@ for (titles of hogwartsHouses){
 
 // 6a
 const quote = [ 
-    `"Yer`,
-     `A`,
-     `Wizard`, 
-     `Harry"`  
+    "Yer",
+     "A",
+     "Wizard", 
+     "Harry"  
 ];
 
+// 6b (Correct answer not mine)
+// let sentence = "";
+// while   (quote !== sentence){
+//     console.log(quote.join (" "));
+//     break;
+// }          OR
+//  Using a FOR OF LOOP
+ let sentence = "";
+ for (word of quote){
+     sentence += `${word} `;
+ }
+ console.log(sentence.trim());
 
+// Using a FOR LOOP
+// let sentence = "";
+// for (i = 0; i <quote.length; i++){
+//     // console.log(quote[i]);
+//     sentence += `${quote[i]} `;
+//     console.log(sentence);
+// }
+// console.log(sentence.trim());
+// putting trim on end of sentence will cut the space at the end of the sentence even though it can't be seen in this example.
